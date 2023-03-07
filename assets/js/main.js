6cd287e68f9ad51649f0e7890f1647b54f971ded
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-03-07 02:24:18 +0200

    sovle animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,51 +169,19 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 // my own js
-// // Wrap every letter in a span
-/**
- * Animation on On logo H1
- */
-var textWrapper = document.querySelector('.ml11 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /([^\x00-\x80]|\w)/g,
-  "<span class='letter'>$&</span>"
-);
 
-anime
-  .timeline({ loop: true })
-  .add({
-    targets: '.ml11 .line',
-    scaleY: [0, 1],
-    opacity: [0.5, 1],
-    easing: 'easeOutExpo',
-    duration: 700,
+document.addEventListener("DOMContentLoaded", function () {
+  new TypeIt("#letters", {
+    speed: 100,
+    startDelay: 900,
+    loop: true
   })
-  .add({
-    targets: '.ml11 .line',
-    translateX: [
-      0,
-      document.querySelector('.ml11 .letters').getBoundingClientRect().width +
-      10,
-    ],
-    easing: 'easeOutExpo',
-    duration: 700,
-    delay: 100,
-  })
-  .add({
-    targets: '.ml11 .letter',
-    opacity: [0, 1],
-    easing: 'easeOutExpo',
-    duration: 600,
-    offset: '-=775',
-    delay: (el, i) => 34 * (i + 1),
-  })
-  .add({
-    targets: '.ml11',
-    opacity: 0,
-    duration: 1000,
-    easing: 'easeOutExpo',
-    delay: 1000,
-  });
+    .type("I Am Saeed", { delay: 3000 })
+    .delete(null)
+    .type("Full Stack Developer", { delay: 2000 })
+    // .delete(null, { speed: 20 })
+    .go();
+});
 
 function sendMail() {
   const subject = document.getElementById('subject').value;
